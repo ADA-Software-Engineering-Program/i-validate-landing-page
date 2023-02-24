8a586329879014a325e8abef60671bafa9fbd37e
Ogochukwu Ugorji <ugorji.ogochukwu@gmail.com>
2023-02-24 20:59:19 +0100

    Created herosection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import React from 'react';
 import './App.css';
+import ResearchCard from './components/ResearchCard';
+import Plan from './components/Plan';
+
+import Hero from './pages/Hero';
 
 function App() {
+  
   return (
     <div className="App">
-      
+      <Hero />
+      <ResearchCard />
+      <Plan/>
     </div>
   );
 }
