db62f82ba6cb14d7777485b1fe7ec78002694ea9
KasiePatricia <ugwukasiep@gmail.com>
2023-02-26 19:44:42 +0100

    contact page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';import React from 'react';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
-import Subsection from './components/subsection/Subsection';
-import ResearchCard from './components/ResearchCard';
-import Plan from './components/Plan';
-
-import Hero from './pages/Hero';
 import Contact from './newContactPage/Contact';
 
 function App() {
   
   return (
-    <div className="App">
-      {/* <Header />
-      <Hero />
-      <ResearchCard />
-      <Plan/>
-      <Subsection />
-      <Footer /> */}
-      <Contact />
-    </div>
+     <Router>
+      <Routes>
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
+   </Router>
   );
 }
 
