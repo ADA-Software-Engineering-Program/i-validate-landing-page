2b3ebe5e06d0e8ef256bcf43d03da7cc6e065f23
KasiePatricia <ugwukasiep@gmail.com>
2023-02-26 23:02:31 +0100

    Adjusted the background

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -1,43 +1,48 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import './Service.css';
+import One from '../images/one.png'
+import { useState } from 'react';
 
-const List = [
-  {
-    title: 'Market Research',
-    img: 'one.png',
-    content:
-      'We offer tools to conduct market research, such as competitor analysis, customer surveys, and focus groups. This information would help you understand the market demand and potential for their idea.This information would help you understand the market demand and potential for their idea',
-    id: 1,
-  },
-  {
-    title: 'Business Plan Generator',
-    img: './two.png',
-    content:
-      'We provide a step-by-step guide to create a business plan, including financial projections, marketing strategies,and a sales plan. This would help users to identify potential challenges and opportunities before investing significant resources.',
-    id: 2,
-  },
-  {
-    title: 'Expert Feedback',
-    img: './three.png',
-    content:
-      'I-Validate connect users with industry experts or perienced entrepreneurs who can offer feedback and advice on their idea. This would provide an objective perspective and help users refine their idea.',
 
-    id: 3,
-  },
-  {
-    title: 'Crowdfunding',
-    img: './.png',
-    content:
-      'We offer a crowdfunding platform to allow users to test their idea by raising funds for a prototype or minimum viable product. This would help users to validate market demand and attract potential investors.',
-    id: 4,
-  },
-];
 
 const Service = () => {
+
+  const [List] = useState( [
+    {
+      title: 'Market Research',
+      img: {One},
+      content:
+        'We offer tools to conduct market research, such as competitor analysis, customer surveys, and focus groups. This information would help you understand the market demand and potential for their idea.This information would help you understand the market demand and potential for their idea',
+      id: 1,
+    },
+    {
+      title: 'Business Plan Generator',
+      img: './two.png',
+      content:
+        'We provide a step-by-step guide to create a business plan, including financial projections, marketing strategies,and a sales plan. This would help users to identify potential challenges and opportunities before investing significant resources.',
+      id: 2,
+    },
+    {
+      title: 'Expert Feedback',
+      img: './three.png',
+      content:
+        'I-Validate connect users with industry experts or perienced entrepreneurs who can offer feedback and advice on their idea. This would provide an objective perspective and help users refine their idea.',
+  
+      id: 3,
+    },
+    {
+      title: 'Crowdfunding',
+      img: './.png',
+      content:
+        'We offer a crowdfunding platform to allow users to test their idea by raising funds for a prototype or minimum viable product. This would help users to validate market demand and attract potential investors.',
+      id: 4,
+    },
+  ]);
+
   return (
     <section className="container mt-5 ml-5 mr-5 pl-5">
-      <h2 className="text-white mb-5 fw-bold"> Service</h2>
+      <h2 className="text-white mb-5 fw-bold"> Service </h2>
       <div className="row">
         {List.map((item) => {
           // const { title, img, content } = item;
