c4ba060cb592f3cfe0af4ae408e13d6caec62b6b
Ibraheem <iejibode@gmail.com>
2023-02-26 20:07:58 +0100

    service

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Contact from './newContactPage/Contact';
+import Service from './Service/Service';
 
 function App() {
-  
   return (
-     <Router>
+    <Router>
       <Routes>
         <Route exact path="/contact" element={<Contact />} />
+        <Service />
       </Routes>
-   </Router>
+    </Router>
   );
 }
 
