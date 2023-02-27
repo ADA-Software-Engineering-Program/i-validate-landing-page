7bf8b0d9f1fceabae0e9900e32dbfb5a9aba30f9
KasiePatricia <ugwukasiep@gmail.com>
2023-02-27 16:43:57 +0100

    fixed the navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,55 +2,42 @@ import React from "react";
 import './headerFooter.css';
 import Logo from '../../images/i-validate-logo-optimiz.jpg';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
+
+   //assigning location variable
+   const location = useLocation();
+
+   //destructuring pathname from location
+   const { pathname } = location;
+ 
+   //Javascript split method to get the name of the path in array
+   const splitLocation = pathname.split("/");
+
+
   return (
-    <nav className="navbar navbar-expand-lg bg-white navbar-box">
-      <div className="container-fluid navbar-changes">
-        <a className="navbar-brand" href={"/"}>
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home"> 
           <img src={Logo} alt="logo" className="headerLogo"/>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href={"/"}>
-                Expert View
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={"/"}>
-                Analytics
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-                <a className="nav-link" href={"/"}>
-                    Blog
-                </a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href={"/"}>
-                Sign Up
-              </a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href={"/"}>
-                Log In
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto nav-gap">
+            <Nav.Link href={"/"}  className={splitLocation[1] === "" ? "active" : ""}>Expert View</Nav.Link>
+            <Nav.Link href={"/contact"} className={splitLocation[1] === "analytics" ? "active" : ""}>Analytics</Nav.Link>
+            <Nav.Link href={"/service"} className={splitLocation[1] === "blog" ? "active" : ""}>Blog</Nav.Link>
+            <Nav.Link href={"/service"} className={splitLocation[1] === "sign up" ? "active" : ""}>Sign Up</Nav.Link>
+            <Nav.Link href={"/service"} className={splitLocation[1] === "log in" ? "active" : ""}>Log In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
