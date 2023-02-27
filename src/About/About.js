aed7c6cb2f93db3d12ae71547ae8e2819bf137fd
KasiePatricia <ugwukasiep@gmail.com>
2023-02-27 10:49:08 +0100

    changed the about page and others

--- src/About/About.js
+++ src/About/About.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import landImg from '../images/business-woman.png';
-import Landherocard from './Landherocard';
+import React from "react";
+import landImg from "../images/business-woman.png";
+import Landherocard from "./Landherocard";
+
+import { Link } from "react-router-dom";
+import { BsGlobe } from "react-icons/bs";
+import Person from "../images/person.png"
+
 
 const About = () => {
-  
+  return (
+    <main>
+      <section className="row">
+        <div className="col-md-6 rightCol">
+          <h1 className="w-lg-50 title text-white Hero-head ">
+            Bring your refined innovative ideas to market.
+          </h1>
+          <button className="btn heroBtn mt-4 text-white">Get Started</button>
+        </div>
+        <div className="col-md-6 position-relative leftCol">
+          <div className="img-arc">
+            <img src={landImg} className="landImg" alt="business woman" />
+          </div>
+          <div className="img-arc arc-right"></div>
+          <div className="small-circle small-circle-main"></div>
+          <div className="small-circle circle-right"></div>
+          <div className="mid-circle"></div>
+        </div>
+      </section>
+      <Landherocard />
+      <div className="d-flex justify-content-between align-items-center container mb-5">
+        <div className="contact-text-color">
+          <BsGlobe />
+          <Link to='/' className="ms-3 contact-text-color text-decoration-none">www.ivalidate.com</Link>
+        </div>
+        <div className="bg-white person-img">
+          <img src={Person} alt="person"/>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-    return (
-      <main>
-        <section className='row'>
-          <div className='col-md-6 rightCol'>
-            <h1  className='w-lg-50 title text-white Hero-head '>
-              Bring your refined innovative ideas to market.
-            </h1>
-            <button className='btn heroBtn mt-4 text-white'>Get Started</button>
-            
-          </div>
-          <div className='col-md-6 position-relative'>
-            <div className='img-arc'>
-            <img src={landImg} className='landImg' alt='business woman' />
-            </div>
-            <div className='img-arc arc-right'></div>
-            <div className='small-circle'></div>
-            <div className='small-circle circle-right'></div>
-            <div className='mid-circle'></div>
-          </div>
-        </section>
-        <Landherocard />
-      <div className='d-flex'>
-      <div>
-        
-      </div>
-      
-      </div>
-        
-      </main>
-        
-        
-        
-      
-    );
-  };
-  
-  export default About;
+export default About;
--- src/About/Navbar.js
+++ src/About/Navbar.js
@@ -1,47 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../images/white logo 2.png';
 import './landing.css'
 
-const Navbar =() => {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import { useLocation } from "react-router-dom";
+
+// const Navbar =() => {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbarBox">
+//       <div className="container-fluid">
+//         <a className="navbar-brand Lo" href="/">
+//           <img src={Logo} alt="logo" className="headerLogo"/>
+//         </a>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarSupportedContent"
+//           aria-controls="navbarSupportedContent"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse navLinkContainer" id="navbarSupportedContent">
+//           <ul className='navbar-nav '>
+//           <li className="nav-item">
+//                 <a className="nav-link active" aria-current="page" href={"/"}>
+//                   About Us
+//                 </a>
+//           </li>
+//           <li className="nav-item">
+//                 <a className="nav-link"  href={"/contact"}>
+//                   Contact Us
+//                 </a>
+//             </li>    
+//             <li className="nav-item">
+//                 <a className="nav-link"  href={"/service"}>
+//                 Services
+//                 </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+
+
+const NavbarComponent = () => {
+
+      //assigning location variable
+      const location = useLocation();
+
+      //destructuring pathname from location
+      const { pathname } = location;
+  
+      //Javascript split method to get the name of the path in array
+      const splitLocation = pathname.split("/");
+
   return (
-    <nav className="navbar navbar-expand-lg navbarBox">
-      <div className="container-fluid">
-        <a className="navbar-brand Lo" href="/">
+
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home"> 
           <img src={Logo} alt="logo" className="headerLogo"/>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse navLinkContainer" id="navbarSupportedContent">
-          <ul className='navbar-nav '>
-          <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href={"/"}>
-                  About Us
-                </a>
-          </li>
-          <li className="nav-item">
-                <a className="nav-link"  href={"/contact"}>
-                  Contact Us
-                </a>
-            </li>    
-            <li className="nav-item">
-                <a className="nav-link"  href={"/service"}>
-                Services
-                </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto nav-gap">
+            <Nav.Link href={"/"}  className={splitLocation[1] === "" ? "active" : ""}>About Us</Nav.Link>
+            <Nav.Link href={"/contact"} className={splitLocation[1] === "contact" ? "active" : ""}>Contact Us</Nav.Link>
+            <Nav.Link href={"/service"} className={splitLocation[1] === "service" ? "active" : ""}>Service</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar
+
+
+export default NavbarComponent
+
